bff2269dde2762ea673b3862d8d4aaa0f86b928b
Jacek Milewski <jacek.milewski@circlekeurope.com>
2019-02-21 15:16:13 +0100

    5: done

--- src/main/java/pl/jm/calc/CalcService.java
+++ src/main/java/pl/jm/calc/CalcService.java
@@ -1,14 +1,18 @@
 package pl.jm.calc;
 
 import lombok.AllArgsConstructor;
+import pl.jm.user.UserApi;
 
 @AllArgsConstructor
 class CalcService {
 
     private CalcValidator calcValidator;
+    private UserApi userApi;
 
     int calcRating(int age) {
-        if (calcValidator.ageIsValid(age)) {
+
+        if (calcValidator.ageIsValid(age)
+                && userApi.verifyUser(age)) {
             return age * 2;
         } else {
             return 0;
--- src/test/java/pl/jm/calc/CalcApiTest.java
+++ src/test/java/pl/jm/calc/CalcApiTest.java
@@ -2,6 +2,7 @@ package pl.jm.calc;
 
 import org.junit.Before;
 import org.junit.Test;
+import pl.jm.user.UserApi;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -11,12 +12,28 @@ import static org.mockito.Mockito.mock;
 public class CalcApiTest {
 
     private CalcApi calcApi;
+    private UserApi userApi;
 
     @Before
     public void setUp() {
+        userApi = mock(UserApi.class);
         calcApi = new CalcApi(
-                new CalcService(new CalcValidator())
+                new CalcService(new CalcValidator(), userApi)
         );
+
+        given(userApi.verifyUser(anyInt())).willReturn(true);
+    }
+
+    @Test
+    public void calculate_returnsRating_forAdultNotAllowedByUserApi() {
+        //given
+        given(userApi.verifyUser(anyInt())).willReturn(false);
+
+        //when
+        int rating = calcApi.calcRating(27);
+
+        //then
+        assertThat(rating).isEqualTo(0);
     }
 
     @Test
