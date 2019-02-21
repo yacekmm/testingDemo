9b630fce7e79c42ef6b628995a5a57c24ed6cbfd
Jacek Milewski <jacek.milewski@circlekeurope.com>
2019-02-21 11:38:57 +0100

    1 init version

--- src/main/java/pl/jm/calc/CalcService.java
+++ src/main/java/pl/jm/calc/CalcService.java
@@ -1,31 +1,5 @@
 package pl.jm.calc;
 
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
-import pl.jm.user.UserApi;
-
-import java.util.UUID;
-
-@Service
-@AllArgsConstructor
 class CalcService {
 
-    private CalcValidator calcValidator;
-    private UserApi userApi;
-    private CalcRepository calcRepository;
-
-    int calculateRating(int age) {
-
-        int rating;
-
-        if (calcValidator.ageIsValid(age)
-                && userApi.verifyUser(age)) {
-            rating = age * 2;
-        } else {
-            rating = 0;
-        }
-
-        calcRepository.save(new CalcEntity(UUID.randomUUID(), age, rating));
-        return rating;
-    }
 }
---
+++ src/test/java/pl/jm/calc/CalcServiceTest.java
@@ -0,0 +1,22 @@
+package pl.jm.calc;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalcServiceTest {
+
+    CalcService calcService;
+
+    @Before
+    public void setUp() {
+        calcService = new CalcService();
+    }
+
+    @Test
+    public void calculate_returnsRating_forAdult() {
+        //when
+
+        //then
+    }
+
+}
